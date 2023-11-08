f0110bc71a55aec4c357ea07551950486c613371
NatalieTanML <natalietan613@gmail.com>
2023-11-08 15:53:45 +0800

    implement task 1

--- Template/A0220822U/topology.py
+++ Template/A0220822U/topology.py
@@ -12,8 +12,31 @@ net = None
 
 class Topology(Topo):
     def build(self):
-        # Add your code here
-        pass
+        # Add the routers and configure their loopback addresses[^1^][1]
+        r110 = self.addNode('r110', cls=FRRRouter, loopback='100.100.1.1/32')
+        r120 = self.addNode('r120', cls=FRRRouter, loopback='100.100.1.2/32')
+        r130 = self.addNode('r130', cls=FRRRouter, loopback='100.100.1.3/32')
+        r210 = self.addNode('r210', cls=FRRRouter, loopback='100.100.2.1/32')
+        r310 = self.addNode('r310', cls=FRRRouter, loopback='100.100.3.1/32')
+        r410 = self.addNode('r410', cls=FRRRouter, loopback='100.100.4.1/32')
+
+        # Add the hosts and configure their IP addresses
+        h211 = self.addHost('h211', ip='10.2.1.1/24')
+        h311 = self.addHost('h311', ip='10.3.1.1/24')
+        h411 = self.addHost('h411', ip='10.4.1.1/25')
+        h412 = self.addHost('h412', ip='10.4.1.129/25')
+
+        # Add the links between the nodes and assign the interface IP addresses
+        self.addLink(r110, r120, intfName1='r110-eth1', intfName2='r120-eth1', params1={'ip': '192.168.1.0/31'}, params2={'ip': '192.168.1.1/31'})
+        self.addLink(r120, r130, intfName1='r120-eth2', intfName2='r130-eth1', params1={'ip': '192.168.1.2/31'}, params2={'ip': '192.168.1.3/31'})
+        self.addLink(r110, r210, intfName1='r110-eth2', intfName2='r210-eth1', params1={'ip': '172.17.1.0/31'}, params2={'ip': '172.17.1.1/31'})
+        self.addLink(r130, r310, intfName1='r130-eth2', intfName2='r310-eth1', params1={'ip': '172.17.2.0/31'}, params2={'ip': '172.17.2.1/31'})
+        self.addLink(r110, r410, intfName1='r110-eth3', intfName2='r410-eth2', params1={'ip': '172.17.3.0/31'}, params2={'ip': '172.17.3.1/31'})
+        self.addLink(r130, r410, intfName1='r130-eth3', intfName2='r410-eth3', params1={'ip': '172.17.4.0/31'}, params2={'ip': '172.17.4.1/31'})
+        self.addLink(r210, h211, intfName1='r210-eth0', intfName2='h211-eth0', params1={'ip': '10.2.1.254/24'})
+        self.addLink(r310, h311, intfName1='r310-eth0', intfName2='h311-eth0', params1={'ip': '10.3.1.254/24'})
+        self.addLink(r410, h411, intfName1='r410-eth0', intfName2='h411-eth0', params1={'ip': '10.4.1.126/25'})
+        self.addLink(r410, h412, intfName1='r410-eth1', intfName2='h412-eth0', params1={'ip': '10.4.1.254/25'})
 
 
 def startNetwork():
